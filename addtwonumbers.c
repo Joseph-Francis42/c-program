34b5708530254b687a7d54699a8424c744638d93
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2025-01-22 16:27:11 +0530

    Add files via upload

---
+++ addtwonumbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  char name[14];
+  printf("enter your name\n");
+  scanf("%s",name);
+  printf("welcome %s",name);
+  
+  return 0;
+}  
---
+++ name.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  char value;
+  printf("enter character capital letter\n");
+  value=getchar();
+  value=value+32;
+  printf("lower case letter:%c\n",value);
+   
+   
+   
+  return 0;
+  
+}  
